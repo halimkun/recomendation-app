3bbbd65644dc18572143da36f6cd19f291339052
halimkun <ffaisalhalim@gmail.com>
2023-01-07 09:53:35 +0700

    ..

--- django_rekomendasi/settings.py
+++ django_rekomendasi/settings.py
@@ -141,10 +141,11 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Media files
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
