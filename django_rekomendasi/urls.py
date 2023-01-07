95f4d7f1da38e55dda6b49333a9fa545631d4a2c
halimkun <ffaisalhalim@gmail.com>
2023-01-07 10:24:27 +0700

    update url

--- django_rekomendasi/urls.py
+++ django_rekomendasi/urls.py
@@ -34,4 +34,7 @@ urlpatterns = [
 
     path("rekomendasi/print/", views.print_rekomendasi, name="print_rekomendasi"),
     path("rekomendasi/mprint/", views.print_mrekomendasi, name="print_mrekomendasi")
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
